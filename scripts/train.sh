ba92295da84ab07635e9167754768b46c61bc24b
dongzhe <142124898+FanDongzhe123@users.noreply.github.com>
2026-06-01 18:07:26 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -21,74 +21,30 @@ if [ ${model} = "vicuna" ]; then
   use_hop=2
   template= ${template}
   projector_type="linear"
-  prefix=graphllava-vicuna-7b-${emb}-${use_hop}-${sample_size}-${projector_type}-${template}-dual-projector-cross-attention-layer-${cross_attn_layers}
+  prefix=mlaga-vicuna-7b-${emb}-${use_hop}-${sample_size}-${projector_type}-${template}-dual-projector-cross-attention-layer-${cross_attn_layers}
   model_base=lmsys/vicuna-7b-v1.5-16k
   mode="v1"
 elif [ ${model} = "vicuna_2layer" ]; then
   use_hop=2
   template=${template}
   projector_type="2-layer-mlp"
-  prefix=graphllava-vicuna-7b-${emb}-${use_hop}-${sample_size}-${projector_type}-${template}-dual-projector-cross-attention-layer-${cross_attn_layers}
+  prefix=mlaga-vicuna-7b-${emb}-${use_hop}-${sample_size}-${projector_type}-${template}-dual-projector-cross-attention-layer-${cross_attn_layers}
   model_base=lmsys/vicuna-7b-v1.5-16k
   mode="v1"
 elif [ ${model} = "vicuna_13b_2layer" ]; then
   use_hop=2
   template=${template}
   projector_type="2-layer-mlp"
-  prefix=graphllava-vicuna-13b-${emb}-${use_hop}-${sample_size}-${projector_type}-${template}-dual-projector-cross-attention-layer-${cross_attn_layers}
+  prefix=mlaga-vicuna-13b-${emb}-${use_hop}-${sample_size}-${projector_type}-${template}-dual-projector-cross-attention-layer-${cross_attn_layers}
   model_base=lmsys/vicuna-13b-v1.5-16k
-  mode="v1"
-elif [ ${model} = "vicuna_4hop" ]; then
-  use_hop=4
-  template="HO"
-  projector_type="linear"
-  prefix=graphllava-vicuna-7b-${emb}-${use_hop}-hop-token-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
-  model_base=lmsys/vicuna-7b-v1.5-16k
-  mode="v1"
-elif [ ${model} = "vicuna_4hop_2layer" ]; then
-  use_hop=4
-  template="HO"
-  projector_type="2-layer-mlp"
-  prefix=graphllava-vicuna-7b-${emb}-${use_hop}-hop-token-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
-  model_base=lmsys/vicuna-7b-v1.5-16k
   mode="v1"
 elif [ ${model} = "llama" ]; then
   use_hop=2
   template=${template}
   projector_type="2-layer-mlp"
-  prefix=graphllava-llama-2-7b-hf-${emb}-${use_hop}-${sample_size}-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
+  prefix=mlaga-llama-2-7b-hf-${emb}-${use_hop}-${sample_size}-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
   model_base=meta-llama/Llama-2-7b-hf
   mode="graphllava_llama_2"
-elif [ ${model} = "llama3" ]; then
-  use_hop=2
-  template=${template}
-  projector_type="2-layer-mlp"
-  prefix=graphllava-llama-3.1-8b-${emb}-${use_hop}-${sample_size}-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
-  model_base=meta-llama/Llama-3.1-8B-Instruct
-  mode="graphllava_llama_3"
-elif [ ${model} = "llama_4hop" ]; then
-  use_hop=4
-  template="HO"
-  projector_type="linear"
-  prefix=graphllava-llama-2-7b-hf-${emb}-${use_hop}-hop-token-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
-  model_base=meta-llama/Llama-2-7b-hf
-  mode="graphllava_llama_2"
-elif [ ${model} = "opt_2.7b" ]; then
-  use_hop=2
-  template="ND"
-  projector_type="linear"
-  prefix=graphllava-opt-2.7b-${emb}-${use_hop}-${sample_size}-${projector_type}-dual-projector-cross-attention-layer-${cross_attn_layers}
-  model_base=facebook/opt-2.7b
-  max_len=1536
-  mode="v1"
-elif [ ${model} = "opt_2.7b_4hop" ]; then
-  use_hop=4
-  template="HO"
-  projector_type="linear"
-  prefix=graphllava-opt-2.7b-${emb}-${use_hop}-hop-token-${projector_type}-only-train-pretrain
-  model_base=facebook/opt-2.7b
-  max_len=1536
-  mode="v1"
 fi
 
 
@@ -104,7 +60,7 @@ echo python  MMGIT/train/train_mem.py \
 --mm_use_graph_start_end False \
 --mm_use_graph_patch_token False \
 --bf16 True \
---output_dir  ./rebuttal_checkpoints/${dataset}/${prefix}_${task}_${epoch} \
+--output_dir  ./checkpoints/${dataset}/${prefix}_${task}_${epoch} \
 --num_train_epochs ${epoch} \
 --per_device_train_batch_size ${bs} \
 --per_device_eval_batch_size 4 \
@@ -143,7 +99,7 @@ python  MMGIT/train/train_mem.py \
 --mm_use_graph_start_end False \
 --mm_use_graph_patch_token False \
 --bf16 True \
---output_dir  ./rebuttal_checkpoints/${dataset}/${prefix}_${task}_${epoch} \
+--output_dir  ./checkpoints/${dataset}/${prefix}_${task}_${epoch} \
 --num_train_epochs ${epoch} \
 --per_device_train_batch_size ${bs} \
 --per_device_eval_batch_size 4 \
